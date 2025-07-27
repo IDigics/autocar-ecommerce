39abe9d3fec444447611535e70fb6ee4ea36b127
Wissem Hajbi <wissemhajbi2002@gmail.com>
2025-07-27 20:25:28 +0200

    setup tanstack useQuery provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import ReactQueryProvider from "../components/useQueryProvider";
 
 import { Cormorant_Garamond } from "next/font/google";
 
@@ -38,9 +39,11 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} ${cormorant.variable} antialiased font-sans bg-[#f8f8f6] text-[#0A0A23]`}
       >
-        <Header />
-        {children}
-        <Footer />
+        <ReactQueryProvider>
+          <Header />
+          {children}
+          <Footer />
+        </ReactQueryProvider>
       </body>
     </html>
   );
